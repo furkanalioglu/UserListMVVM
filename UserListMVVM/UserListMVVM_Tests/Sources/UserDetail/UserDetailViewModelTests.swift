d8521f9efebf19f0e6a7e2ff7e76fcbb2eb0e18e
Furkan <furkanalioglu@gmail.com>
2025-02-16 13:26:46 +0300

    feat: User Detail Tests.

---
+++ UserListMVVM/UserListMVVM_Tests/Sources/UserDetail/UserDetailViewModelTests.swift
@@ -0,0 +1,95 @@
+//
+//  UserDetailViewModelTests.swift
+//  UserListMVVM_Tests
+//
+//  Created by furkan on 16.02.2025.
+//
+
+import XCTest
+import Combine
+@testable import UserListMVVM
+
+final class UserDetailViewModelTests: XCTestCase {
+    // MARK: - Properties
+    private var disposeBag: Set<AnyCancellable>!
+    
+    // MARK: - Setup
+    override func setUp() {
+        super.setUp()
+        disposeBag = []
+    }
+    
+    override func tearDown() {
+        disposeBag = nil
+        super.tearDown()
+    }
+    
+    // MARK: - Initial State Tests
+    func test_InitialState_ShouldSetupCorrectSections() {
+        // Given
+        let mockUser = UserBuilder.buildMockUsers().first!
+        let sut = UserDetailViewModel(user: mockUser)
+        let expectation = expectation(description: "Should setup correct sections")
+        
+        // When
+        sut.state
+            .sink { state in
+                if case let .loaded(sections) = state {
+                    // Then
+                    XCTAssertEqual(sections.count, 4, "Should have 4 sections")
+                    
+                    // Verify header section
+                    if case let .header(headerViewModel) = sections[0] {
+                        XCTAssertEqual(headerViewModel.name, mockUser.name)
+                        XCTAssertEqual(headerViewModel.username, mockUser.username)
+                    } else {
+                        XCTFail("First section should be header")
+                    }
+                    
+                    // Verify contact section
+                    if case let .contact(contactViewModels) = sections[1] {
+                        XCTAssertEqual(contactViewModels.count, 3)
+                    } else {
+                        XCTFail("Second section should be contact")
+                    }
+                    
+                    // Verify address section
+                    if case let .address(addressViewModels) = sections[2] {
+                        XCTAssertEqual(addressViewModels.count, 3)
+                    } else {
+                        XCTFail("Third section should be address")
+                    }
+                    
+                    // Verify company section
+                    if case let .company(companyViewModels) = sections[3] {
+                        XCTAssertEqual(companyViewModels.count, 3)
+                    } else {
+                        XCTFail("Fourth section should be company")
+                    }
+                    
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &disposeBag)
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    // MARK: - Memory Management Tests
+    func test_MemoryLeaks_ShouldNotOccur() {
+        // Given
+        weak var weakSut: UserDetailViewModel?
+        
+        autoreleasepool {
+            let mockUser = UserBuilder.buildMockUsers().first!
+            let sut = UserDetailViewModel(user: mockUser)
+            weakSut = sut
+            
+            // When - Force a state update
+            _ = sut.state.sink { _ in }
+        }
+        
+        // Then
+        XCTAssertNil(weakSut, "ViewModel should be deallocated")
+    }
+}
